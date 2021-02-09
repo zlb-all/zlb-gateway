ab9906d44bc8b519ff8aaf841817e59ffcc74764
zhoulibin <zhoulb@chinaexpressair.com>
2021-02-09 15:52:23 +0800

    zlb-gateway 创建

---
+++ zlb-gateway-service/src/main/java/com/zlb/ZlbGatewayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.zlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoulibin
+ */
+@SpringBootApplication
+public class ZlbGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlbGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ zlb-gateway-service/src/test/java/com/zlb/ZlbGatewayServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.zlb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class ZlbGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertFalse(false,"");
+    }
+
+}
